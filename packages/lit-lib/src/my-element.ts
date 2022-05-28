9a940ee8f12ddffc958e8aae0128c89bca9aa276
Dmytro Zemnytskyi <pragmasoft@gmail.com>
2022-05-28 14:02:22 +0300

    played with tailwind and shared styles, unsuccessfully for now

--- packages/lit-lib/src/my-element.ts
+++ packages/lit-lib/src/my-element.ts
@@ -1,14 +1,17 @@
 import { LitElement, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
+import { sharedStyles } from "./shared-styles";
 
 @customElement("my-element")
 export class MyElement extends LitElement {
-  @property()
+  static styles = sharedStyles;
+
+  @property({ type: String })
   version = "STARTING";
 
   render() {
     return html`
-      <p>Welcome to the Lit tutorial!</p>
+      <p class="text-blue-600">Welcome to the web components tutorial!</p>
       <p>This is the ${this.version} code.</p>
     `;
   }
---
+++ packages/lit-lib/src/shared-styles.ts
@@ -0,0 +1,5 @@
+/// <reference path="index.d.ts"/>
+import { css } from "lit";
+import s from "bundle-text:./shared-styles.css";
+const a = [s];
+export const sharedStyles = css({ ...a, raw: a });
